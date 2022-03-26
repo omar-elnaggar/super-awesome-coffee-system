26dd604f1ca3083e7b55ea04ecf2b20246eaa429
omar.elnaggar <elnaggaros@gmail.com>
2022-03-26 16:53:18 -0700

    added clock display and setting of the clock

--- SACSController.cpp
+++ SACSController.cpp
@@ -1,6 +1,9 @@
 #include "SACSController.h"
 #include <iostream>
 
+#define MINUTE_VALUE 60
+#define HOUR_VALUE 3600
+
 // setters
 void SACSController::setTemp(int temperature){
     this->temp = temperature;
@@ -14,10 +17,23 @@ void SACSController::setAutoTemp(int temperature){
 void SACSController::setBrewing(bool brewing){
     this->brewing = brewing;
 }
+void SACSController::setClock(Clock &clock, int seconds){
+    if (seconds > 86400){
+        throw std::runtime_error("Too many seconds");
+    } else {
+        clock.setSeconds(seconds);
+    }
+}
 
 // display functions
 void SACSController::displayTime(){
-    // to be implemented when clock is updated
+    // 3600 seconds in an hour 60 seconds in a minute
+    int seconds = clock.getSeconds();
+    int hours = seconds / HOUR_VALUE;
+    seconds -= hours * HOUR_VALUE;
+    int minutes = seconds / MINUTE_VALUE;
+
+    std::cout << hours << ":" << minutes << std::endl;
 }
 void SACSController::displayMenu(){
     // to be implemented
